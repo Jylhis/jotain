bfa9b2c011f4341bb9a423311ab051727e7b7e81
Markus Jylhänkangas <markus@jylhis.com>
2026-04-02 13:38:01 +0200

    Configure init.el: custom-file, modus theme, core settings, nix-mode

--- init.el
+++ init.el
@@ -7,5 +7,28 @@
 
 (message "Start of Jotain")
 
+;; Store automatic customization options elsewhere
+;; TODO: Where should this go?
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file) (load custom-file))
+
+(load-theme 'modus-operandi-tritanopia t)
+
+(use-package emacs
+  :custom
+  (fill-column 100)
+  (use-short-answers t)
+  (use-dialog-boxes nil)
+  (delete-by-moving-to-trash t)
+  :bind
+  (("C-z" . nil)
+   ("C-x C-z" . nil)
+   ("M-o" . other-window))
+  )
+
+(use-package nix-mode
+  :ensure t
+  :mode "\\.nix\\'")
+
 (provide 'init)
 ;;; init.el ends here
