b12310f6b385025366913ef9f20215c2e76c23f5
Markus Jylhänkangas <markus@jylhis.com>
2026-04-02 13:37:27 +0200

    Add early-init.el with toolbar, startup screen, and use-package settings

---
+++ early-init.el
@@ -0,0 +1,24 @@
+;;; early-init.el --- Early Emacs initialization -*- lexical-binding: t; -*-
+
+;; Author: Markus Jylhänkangas <markus@jylhis.com>
+
+;;; Commentary:
+;; Early initialization file for Emacs 30+.
+;; This file is loaded before package initialization and GUI setup.
+
+;;; Code:
+
+(message "Early start of Jotain")
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+(setq use-package-enable-imenu-support t)
+
+(tool-bar-mode -1)
+
+(setopt inhibit-startup-screen t)
+
+(provide 'early-init)
+;;; early-init.el ends here
