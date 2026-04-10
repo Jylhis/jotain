bcb2750b43845baedb053d9d7131b93fb753b5b0
Markus Jylhänkangas <markus@jylhis.com>
2026-04-10 19:00:52 +0200

    Add per-system packages output to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,13 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/4c1018dae018162ec878d42fec712642d214fdfa";
+
   };
 
   outputs =
     { self, nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+    in
     {
       overlays.default = import ./overlay.nix;
 
       homeManagerModules.default = import ./module.nix;
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.jotainEmacsPackages;
+          emacs = pkgs.jotainEmacs;
+        }
+      );
     };
 }
